47ce586698bec1858235b24e92e395a25088946f
Emil Fabricius Svansø <efs@emilsvansoe.com>
2012-02-21 17:40:21 +0100

    Xresorources

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gitconfig  tmux.conf  vim  vimrc  Xdefauls  xinitrc  Xmodmap  xmonad  zshrc"
+files="gitconfig  tmux.conf  vim  vimrc  Xresources  xinitrc  Xmodmap  xmonad  zshrc"
 
 ##########
 
